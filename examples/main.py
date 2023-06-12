b6ab9024bd8fa6a22576665c59e6127952f652de
rn3r <102843294+rn3r@users.noreply.github.com>
2023-06-12 07:35:44 -0500

    Update main.py
    client update

--- examples/main.py
+++ examples/main.py
@@ -1,27 +1,23 @@
-import asyncio
 import discord
 from underground.gateway.ws import GatewayClient
 from underground.gateway.client import UserClient
 import json
 
-async def main(client):
-    gateway = GatewayClient("wss://underground.haydar.dev/?encoding=json&v=9")
-    await gateway.connect()
+with open("./config.json", "r") as f:
+    config = json.load(f)
 
-    task1 = asyncio.create_task(client.recieve_events(gateway, client))
-    task2 = asyncio.create_task(gateway.start_ping())
-
-    await asyncio.gather(task1, task2)
-
-client = UserClient("UNDERGROUND_CLIENT_TOKEN")
+client = UserClient(config["token"], prefix="!")
 
 @client.command("help")
 async def help(ctx):
-    await ctx.send("https://underground.haydar.dev/invite/i", embed=discord.Embed(description=">>> ```bf\n!reply <message>\n```", color=0x000000))
+    await ctx.send("https://underground.haydar.dev/invite/i", embed=discord.Embed(description="Want to contribute to this open source project? Feel free to make a pull request at https://github.com/rn3r/underground.py\n>>> ```bf\n!reply <message>\n```", color=0x000000))
 
 @client.command("reply")
 async def reply(ctx, message):
     await ctx.reply(embed=discord.Embed(title="Reply", description=message, color=0x00ff00))
 
+@client.command("invite")
+async def invite(ctx):
+    await ctx.send(embed=discord.Embed(title="Invite me to your server", description="support: https://underground.haydar.dev/invite/i\nhttps://underground.haydar.dev/api/oauth2/authorize?client_id=1117555508107436742&permissions=8&scope=bot", color=0x000000))
 
-asyncio.run(main(client))
+client.run()
